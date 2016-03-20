b458e4d69d9b7de4fc6f9538eb3abdd3877bee24
svejaka <kocetons@abv.bg>
2016-03-20 11:03:54 +0200

    Second commit

---
+++ LSYS/src/com/lsys/db/DBConnect.java
@@ -0,0 +1,44 @@
+package com.lsys.db;
+
+import java.sql.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Konstantin
+ */
+public class DBConnect {
+    
+    private static final String DRIVER = "org.postgresql.Driver";
+    private static final String DB_URL = "jdbc:postgresql://localhost:5433/TestDB";
+    private static final String USER = "postgres";
+    private static final String PASS = "masterx";
+    public static Connection conn = null;
+
+    public static Connection createConnection() throws Exception {
+        try {
+            Class.forName(DRIVER).newInstance();
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            JOptionPane.showMessageDialog(null, "Database connected sucessfully");
+            return conn;
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Something went wrong");
+        }
+        return null;
+    }
+
+    public void shutdown() {
+        try {
+//            if (stmt != null) {
+//                stmt.close();
+//            }
+            if (conn != null) {
+                DriverManager.getConnection(DB_URL, USER, PASS + ";shutdown=true");
+                conn.close();
+            }
+        } catch (SQLException ex) {
+//            ex.printStackTrace();
+        }
+        System.out.println("Database connection closed");
+    }
+}
